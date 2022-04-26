02d5a349dc2ca39af53fd5f54866bacb72b1818f
MuhireHubert <hmuhire00@gmail.com>
2022-04-26 08:35:07 -0700

    header

---
+++ alx-low_level_programming/main.h
@@ -0,0 +1,9 @@
+char _putchar(char c);
+char *_memset(char *s, char b, unsigneed int n);
+char *_memcpy(char *dest, vhar *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strpn(char *s, char *accept);
+char *_strpbrk(char *s, char *needle);
+void print_chessboar(char (*a)[8];
+		     void print_diagsums(int *a, int size);
+		     void set_string(char **s, char *to);
